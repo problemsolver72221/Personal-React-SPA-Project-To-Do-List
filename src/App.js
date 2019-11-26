1e99b627b6d8427a503e97f65df1f8e9f00705f3
problemsolver72221 <problemsolver72221@gmail.com>
2019-11-26 17:22:14 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import ToDoPage from "./pages/ToDoPage/ToDoPage";
 import "./App.css";
 
 class App extends Component {
-  // state = {  }
   render() {
     return (
-      <div className="App">
-        <Switch>
-          <Route path="/app" component={ToDoPage} />
-          <Route path="/" component={HomePage} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/app" component={ToDoPage} />
+        <Route path="/" component={HomePage} />
+        <Redirect to="/" />
+      </Switch>
     );
   }
 }
